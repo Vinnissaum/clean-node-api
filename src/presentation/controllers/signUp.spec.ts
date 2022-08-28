7aa28387465eaf2c6ec70e99bfa38ee5ab20e81c
Vinicius Braz <vinicius_lsb@live.com>
2022-08-28 19:07:08 -0300

    feat: ensure SignUp returns 400 if no password is provided

--- src/presentation/controllers/signUp.spec.ts
+++ src/presentation/controllers/signUp.spec.ts
@@ -31,4 +31,19 @@ describe('SignUp controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
+
+  test('Should return 400 if no password is provided', () => {
+    // sut -> system under test
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'test',
+        email: 'test@test.com',
+        passwordConfirmation: 'test'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
 })
