dd45f89cdd0c9ef0872c4c55b3eac98aab8ae485
Vinicius Braz <vinicius_lsb@live.com>
2022-08-28 21:48:03 -0300

    refactor: create an index file to export all protocols

--- src/presentation/controllers/signUp.spec.ts
+++ src/presentation/controllers/signUp.spec.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from './SignUpController'
 import { InvalidParamError, MissingParamError, ServerError } from '../errors'
-import { EmailValidator } from '../protocols/email-validator'
+import { EmailValidator } from '../protocols'
 
 interface makeSutTypes {
   sut: SignUpController
